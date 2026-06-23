e9aa7aa38e058ac838aada335f06e411c9a8de0b
Nate Holcomb <near0837@gmail.com>
2026-06-22 22:52:52 -0400

    add blog shit

---
+++ blog/blogLanding.php
@@ -0,0 +1,12 @@
+<html lang="en">
+  <!-- begin head --> 
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>hey, it's that keycat guy!</title>
+    <link rel="stylesheet" href="/style/blogStyle.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src = "/script/blogScript.js"></script>
+  </head>
+
+  <div 
